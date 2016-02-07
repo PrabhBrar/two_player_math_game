335c4dd23366e5e2c34aa4979d288e96c421d4c4
Prabhsharan Singh Brar <PrabhBrar@users.noreply.github.com>
2016-02-07 03:08:32 -0800

    Update question.rb

--- question.rb
+++ question.rb
@@ -2,21 +2,32 @@ class Question
   attr_reader :ques, :correct_answer
 
   def initialize 
-    @ques = ""
     @correct_answer = nil
     @operator = ["+", "-", "*", "/"]
   end
 
   # A String is generated, which is used as the question.
-  # Depending on the count, it decides if player 1 or player 2 will answer the question.
+  # Always returns a String.
   def generate_question
-    @num1 = rand(1..20)
-    @num2 = rand(1..20)
-    @num3 = rand(0..3)
-    @ques = "What does #{@num1} #{@operator[@num3]} #{@num2} equal?"
+    generate_numbers
+    generate_operator
+    generate_answer
+    "What does #{@num1} #{@operator[@num3]} #{@num2} equal?"
   end
 
-  # The correct answer to the generated question if calculated.
+  def generate_numbers
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+  end
+
+  # One random Integer is generated, which is used to select the operator.
+  # Always returns a Integer.
+  def generate_operator
+    @num3 = rand(0..3)
+  end
+
+  # The correct answer to the generated question is calculated.
+  # Always returns a Integer.
   def generate_answer
     @correct_answer = case @operator[@num3]
                       when "+"
